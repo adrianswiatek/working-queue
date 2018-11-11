30cf98c426db78b5dbbe109ce62c0c52e8af7194
Adrian Świątek <adrian@aswiatek.pl>
2018-11-11 22:44:54 +0100

    Configured AppDelegate

--- WorkingQueue/AppDelegate.swift
+++ WorkingQueue/AppDelegate.swift
@@ -5,12 +5,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
-        
+
+        let controller = SingleQueueController()
+        let navigationController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = navigationController
+
         return true
     }
 }
