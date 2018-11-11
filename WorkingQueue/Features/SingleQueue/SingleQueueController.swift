2ef6ca3dbe4414cbfc0815a6ac3cfb472b2a9a8e
Adrian Świątek <adrian@aswiatek.pl>
2018-11-11 22:55:09 +0100

    Added 'add' button

--- WorkingQueue/Features/SingleQueue/SingleQueueController.swift
+++ WorkingQueue/Features/SingleQueue/SingleQueueController.swift
@@ -4,6 +4,15 @@ class SingleQueueController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAdd))
+    }
+
+    @objc private func handleAdd(button: UIBarButtonItem) {
+        print("Add new item")
     }
 }
