4f6257e0da60428bac173deb95f1cffe3b6de33a
Adrian Świątek <adrian@aswiatek.pl>
2018-11-11 21:45:22 +0100

    Configured window in AppDelegate.swit

--- WorkingQueue/ViewController.swift
+++ WorkingQueue/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  WorkingQueue
-//
-//  Created by Adrian Świątek on 11/11/2018.
-//  Copyright © 2018 Adrian Świątek. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
