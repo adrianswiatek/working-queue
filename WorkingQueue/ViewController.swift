3a13323f78e625e7702b36b8bad85855b22ee0a5
Adrian Świątek <adrian@aswiatek.pl>
2018-11-11 21:40:09 +0100

    Initial commit

---
+++ WorkingQueue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingQueue
+//
+//  Created by Adrian Świątek on 11/11/2018.
+//  Copyright © 2018 Adrian Świątek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
