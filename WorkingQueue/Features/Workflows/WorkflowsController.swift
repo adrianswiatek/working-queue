1a2067e99fb2bd6b6caa463e45188b12f0f2f9ba
Adrian Świątek <adrian@aswiatek.pl>
2018-12-18 18:16:20 +0100

    Added basic table view to WorkflowsController

--- WorkingQueue/Features/Workflows/WorkflowsController.swift
+++ WorkingQueue/Features/Workflows/WorkflowsController.swift
@@ -2,10 +2,59 @@ import UIKit
 
 class WorkflowsController: UIViewController {
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.allowsSelection = false
+        tableView.separatorColor = .separatorColor
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    private let cellIdentifier: String
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        cellIdentifier = "WorkflowCell"
+
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Workflow"
+        setupViews()
+    }
+
+    private func setupViews() {
+        navigationItem.title = "Workflow"
         view.backgroundColor = .backgroundColor
+
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension WorkflowsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = indexPath.row.description
+        return cell
     }
 }
