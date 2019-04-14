5ad5322d2c132b82101979cbbd66ad305dec7ea5
Adrian Świątek <adrian@aswiatek.pl>
2019-04-14 22:10:05 +0200

    Added RxSwift to FinishedItemsController

--- WorkingQueue/Features/FinishedItems/FinishedItemsController.swift
+++ WorkingQueue/Features/FinishedItems/FinishedItemsController.swift
@@ -1,14 +1,16 @@
 import UIKit
-
-public protocol FinishedItemsControllerDelegate {
-    func didDelete(viewController: FinishedItemsController)
-}
+import RxSwift
 
 public class FinishedItemsController: UIViewController {
 
     public var entry: WorkflowEndEntry?
-    public var delegate: FinishedItemsControllerDelegate?
 
+    public var entryObservable: Observable<WorkflowEndEntry> {
+        return entrySubject.asObservable().share()
+    }
+
+    private let entrySubject = PublishSubject<WorkflowEndEntry>()
+    private let disposeBag = DisposeBag()
     private let cellIdentifier = "Cell"
 
     private let toolbar: UIToolbar = {
@@ -22,6 +24,7 @@ public class FinishedItemsController: UIViewController {
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.backgroundColor = .backgroundColor
+        tableView.isEditing = false
         tableView.tableFooterView = UIView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
@@ -30,6 +33,7 @@ public class FinishedItemsController: UIViewController {
     public override func viewDidLoad() {
         configureTableView()
         configureToolbar()
+        setupEntrySubject()
         setupViews()
     }
 
@@ -66,15 +70,15 @@ public class FinishedItemsController: UIViewController {
         alertController.addAction(cancelAction)
 
         let removeAllAction = UIAlertAction(title: "Remove All", style: .destructive) { [weak self] _ in
-            guard let `self` = self else { return }
+            guard let `self` = self, var entry = self.entry else { return }
 
-            let numberOfEntries = self.entry?.numberOfEntries ?? 0
+            let numberOfEntries = entry.numberOfEntries
             let indexPaths = (0 ..< numberOfEntries).map { IndexPath(row: $0, section: 0) }.sorted { $0 < $1 }
 
-            self.entry?.removeAllEntries()
-
+            entry.removeAllEntries()
+            
+            self.entrySubject.onNext(entry)
             self.tableView.deleteRows(at: indexPaths, with: .automatic)
-            self.delegate?.didDelete(viewController: self)
 
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: { [weak self] in
                 self?.navigationController?.popViewController(animated: true)
@@ -83,6 +87,10 @@ public class FinishedItemsController: UIViewController {
         alertController.addAction(removeAllAction)
 
         present(alertController, animated: true)
+    }
+
+    private func setupEntrySubject() {
+        entryObservable.subscribe(onNext: { [weak self] in self?.entry = $0 }).disposed(by: disposeBag)
     }
 
     private func setupViews() {
@@ -121,16 +129,15 @@ extension FinishedItemsController: UITableViewDelegate {
     public func tableView(
         _ tableView: UITableView,
         trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (action, sourceView, completionHandler) in
-            guard let `self` = self, let entry = self.entry else {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, completionHandler) in
+            guard let `self` = self, var entry = self.entry else {
                 completionHandler(false)
                 return
             }
 
             let entryToRemove = entry.getEntries()[indexPath.row]
             entry.removeEntry(entryToRemove)
-
-            self.delegate?.didDelete(viewController: self)
+            self.entrySubject.onNext(entry)
 
             completionHandler(true)
             self.tableView.reloadData()
